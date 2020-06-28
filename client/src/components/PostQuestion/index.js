96b71ed122ddc6ae4e1844cd15ffaf59afd61351
Anna Panas <annapanas0906@gmail.com>
2020-06-28 18:45:58 -0400

    users can post questions. Questions saved to db

--- client/src/components/PostQuestion/index.js
+++ client/src/components/PostQuestion/index.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./style.css";
+import API from "../../utils/API";
 
 const PostQuestion = () => {
+  const [formObject, setFormObject] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormObject({ ...formObject, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    API.saveQuestionToDb(formObject)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
   return (
     <>
       <div className="container">
@@ -21,7 +39,7 @@ const PostQuestion = () => {
                     Be specific, so people can understand the issue you're
                     having
                   </Form.Text>
-                  <Form.Control name="title" />
+                  <Form.Control name="title" onChange={handleInputChange} />
                 </Form.Group>
 
                 <Form.Group controlId="exampleForm.ControlTextarea1">
@@ -30,7 +48,12 @@ const PostQuestion = () => {
                     Include all the information someone would need to answer
                     your question
                   </Form.Text>
-                  <Form.Control as="textarea" rows="11" />
+                  <Form.Control
+                    as="textarea"
+                    rows="11"
+                    name="body"
+                    onChange={handleInputChange}
+                  />
                 </Form.Group>
 
                 <Form.Group controlId="exampleForm.ControlTextarea1">
@@ -38,10 +61,19 @@ const PostQuestion = () => {
                   <Form.Text className="text-muted">
                     Add your code here
                   </Form.Text>
-                  <Form.Control as="textarea" rows="6" />
+                  <Form.Control
+                    as="textarea"
+                    rows="6"
+                    name="code"
+                    onChange={handleInputChange}
+                  />
                 </Form.Group>
               </Form>
-              <Button variant="outline-info" className="post-question">
+              <Button
+                variant="outline-info"
+                className="post-question"
+                onClick={handleSubmit}
+              >
                 Post Question
               </Button>
             </Card>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,6 +4,6 @@ export default {
   saveQuestionToDb: (question) => {
     console.log("api route hit");
     console.log(question);
-    return axios.post("/questions", question);
+    return axios.post("/api/questions", question);
   },
 };
