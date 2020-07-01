226d43838ab368f2e015889508cd1ca1772e6f38
Anna Panas <annapanas0906@gmail.com>
2020-07-01 16:00:47 -0400

    heroku

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
