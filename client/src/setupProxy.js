f34e2f8cb957d1402af5eab7bbea19139e030ef4
Anna Panas <annapanas0906@gmail.com>
2020-07-21 21:20:06 -0400

    heroku

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:3001",
+      target: "http://localhost:3001 || https://coding-forum.herokuapp.com/",
       changeOrigin: true,
     })
   );
