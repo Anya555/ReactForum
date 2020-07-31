a3c2a051e7596b448166e72b7f9cf7982ae00218
Anna Panas <annapanas0906@gmail.com>
2020-07-31 19:36:32 -0400

    heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
 }
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
