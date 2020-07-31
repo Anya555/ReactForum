e2fdaca45efa24bfaf6d814ccebc6684d8548555
Anna Panas <annapanas0906@gmail.com>
2020-07-31 19:31:57 -0400

    heroku

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path_join(__dirname, "./client/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
 }
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 // force:true is essentially the same as DROP DATABASE IF EXISTS ///
 db.sequelize.sync().then(function () {
