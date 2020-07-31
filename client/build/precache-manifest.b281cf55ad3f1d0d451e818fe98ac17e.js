35892fdcbbc3970428cdd68ca0d2dbfac74a1a0e
Anna Panas <annapanas0906@gmail.com>
2020-07-30 21:53:44 -0400

    heroku

---
+++ client/build/precache-manifest.b281cf55ad3f1d0d451e818fe98ac17e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ba06e7ad500cd76fd92760e219d1cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bbcd5ef0795a920f0e0",
+    "url": "/static/css/main.329be978.chunk.css"
+  },
+  {
+    "revision": "d19804d90405e9ae2bc9",
+    "url": "/static/js/2.1668ef68.chunk.js"
+  },
+  {
+    "revision": "5ad387cd57eb31f6f50768be47fe3b09",
+    "url": "/static/js/2.1668ef68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bbcd5ef0795a920f0e0",
+    "url": "/static/js/main.183258ca.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
