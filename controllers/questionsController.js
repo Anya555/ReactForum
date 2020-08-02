629f005ad96545ca84fe8ef33c9c09f143d89d5f
Anna Panas <annapanas0906@gmail.com>
2020-08-02 13:54:41 -0400

    heroku

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -3,28 +3,28 @@ const db = require("../models");
 module.exports = {
   // Post question to db
   create: function (req, res) {
-    db.Questions.create(req.body)
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
+    // db.Questions.create(req.body)
+    //   .then((dbModel) => res.json(dbModel))
+    //   .catch((err) => res.status(422).json(err));
   },
 
   // Get all questions from db
   findAll: function (req, res) {
-    db.Questions.findAll(req.query)
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
+    // db.Questions.findAll(req.query)
+    //   .then((dbModel) => res.json(dbModel))
+    //   .catch((err) => res.status(422).json(err));
   },
 
   updateQuestionData: function (req, res) {
-    db.Questions.update(req.body, { where: { id: req.params.id } })
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
+    // db.Questions.update(req.body, { where: { id: req.params.id } })
+    //   .then((dbModel) => res.json(dbModel))
+    //   .catch((err) => res.status(422).json(err));
   },
 
   // Get all questions from db
   findQuestionId: function (req, res) {
-    db.Questions.findByPk(req.params.id)
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
+    // db.Questions.findByPk(req.params.id)
+    //   .then((dbModel) => res.json(dbModel))
+    //   .catch((err) => res.status(422).json(err));
   },
 };
